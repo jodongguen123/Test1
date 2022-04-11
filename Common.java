a8df0112bcdeffe41832c4c5c5a37e041667c313
JoDongGeun <jokun2006@naver.com>
2022-04-11 13:41:39 +0900

    공통

---
+++ Common.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Common {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("1. 회원가입  2. 로그인  3. 아이디 찾기  4. 비밀번호 찾기");
+		int num = scan.nextInt();
+		if(num == 1) {
+			
+		}else if(num == 2) {
+			
+		}else if(num == 3) {
+			
+		}else if(num == 4){
+			
+		}else {
+			System.out.println("다시 입력하세요");
+		}
+	}
+
+}
